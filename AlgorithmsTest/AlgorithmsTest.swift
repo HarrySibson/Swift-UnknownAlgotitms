cacbd3a342b2f9e3ae51aae8590a8f2f3ab9dbc6
Harry Sibson <Sibson.h@etoncollege.org.uk>
2021-11-30 11:14:53 +0000

    Finished first algorithm and started work on the second

--- AlgorithmsTest/AlgorithmsTest.swift
+++ AlgorithmsTest/AlgorithmsTest.swift
@@ -12,10 +12,21 @@ class AlgorithmsTest: XCTestCase {
         
         let unknownAlgorithms = UnknownAlgorithms()
         
-        let testCases = [(input: [1,2,3,4,5,6],expected: [7]),(input: [12,23,34,45,56],expected: [68])]
+        let testCases = [(input: [1,2,3,4,5,6],expected: 7),(input: [12,23,34,45,56],expected: 68)]
         
         for testCase in testCases {
             let actual = unknownAlgorithms.sumFirstAndLast(testCase.input)
+            XCTAssertEqual(actual,testCase.expected)
+        }
+        
+    }
+    func testFindModeWithSortedArrays(){
+        let unknownAlgorithms = UnknownAlgorithms()
+        
+        let testCases = [(input: [1,2,3,3,3,4,5,6],expected: 3),(input: [12,23,34,34,34,45,45,56],expected: 34)]
+        
+        for testCase in testCases {
+            let actual = unknownAlgorithms.findMode(testCase.input)
             XCTAssertEqual(actual,testCase.expected)
         }
         
--- UnknownAlgorithms/UnknownAlgorithms.swift
+++ UnknownAlgorithms/UnknownAlgorithms.swift
@@ -7,12 +7,51 @@
 
 import Foundation
 class UnknownAlgorithms{
-    func sumFirstAndLast(_ data:[Int])->[Int]{
-        if data.count
+    func sumFirstAndLast(_ data:[Int])->Int{
+        if data.count == 0{
+            return 0
+        }else if data.count == 1{
+            return 2
+        }else{
+            let first = data[0]
+            var sum = data[data.count-1]
+            sum = sum + first
+            return sum
+        }
         
 
-        return [7]
-
+    }
+    func findMode(_ data: [Int])-> Int{
+        var counter = 0
+        var mode = 0
+        var counter2 = 0
+        var mode2 = 0
+        var index = 0
+        if data.count == 0{
+            return 0
+        }else if data.count == 1{
+            return data[0]
+        }else{
+            for i in 0..<data.count-1{
+                if data[i] == data[i+1]{
+                    counter=counter+1
+                    mode = data[i]
+                    index = i-1
+                }
+            }
+            for i in 0..<index-1{
+                if data[i] == data[i+1]{
+                    counter2=counter2+1
+                    mode2 = data[i]
+                }
+            }
+            if counter2 < counter{
+                return mode
+            }else{
+                return mode2
+            }
+        }
+        
     }
 
 }
